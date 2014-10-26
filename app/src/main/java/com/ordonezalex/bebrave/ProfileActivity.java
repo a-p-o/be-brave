59a40c1a7717e9477dff5f49536a47980cbebe18
Sebastian Florez <sflorez@stetson.edu>
2014-10-25 20:04:51 -0400

    added rest functionality to pull a user's profile information

--- app/src/main/java/com/ordonezalex/bebrave/ProfileActivity.java
+++ app/src/main/java/com/ordonezalex/bebrave/ProfileActivity.java
@@ -1,11 +1,27 @@
 package com.ordonezalex.bebrave;
 
 import android.app.Activity;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.ArrayList;
 
 
 public class ProfileActivity extends Activity {
@@ -13,15 +29,26 @@ public class ProfileActivity extends Activity {
     private TextView usernameTextView;
     private TextView userEmailTextView;
     private TextView userIdTextView;
+    private HttpClient client;
+    private JSONObject json;
+    private final static String URL = "http://caffeinatedcm-001-site3.smarterasp.net/api/v0/user";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
 
+        //initialize the text views that we are going to work with
         usernameTextView = (TextView) findViewById(R.id.user_name_text_view);
         userEmailTextView = (TextView) findViewById(R.id.user_email_text_view);
         userIdTextView = (TextView) findViewById(R.id.user_id_text_view);
+
+        //set up the http client for rest requests
+        client = new DefaultHttpClient();
+        ArrayList<String> fields = new ArrayList<String>();
+        fields.add("Username");
+        fields.add("OrganizationIdentifier");
+        new Read().execute(fields);
     }
 
 
@@ -42,5 +69,56 @@ public class ProfileActivity extends Activity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    public JSONObject profileInformation(String username) throws ClientProtocolException, IOException, JSONException
+    {
+        StringBuilder url = new StringBuilder(URL);
+
+        HttpGet get = new HttpGet(url.toString());
+        get.setHeader("Content-Type" , "application/json");
+        HttpResponse response = client.execute(get);
+        int status = response.getStatusLine().getStatusCode();
+
+        if ( status == 200)
+        {
+            HttpEntity entity = response.getEntity();
+            String data = EntityUtils.toString(entity);
+            JSONArray profiledata = new JSONArray(data);
+            JSONObject profile = profiledata.getJSONObject(0);
+            return profile;
+        }
+        else
+        {
+            Toast.makeText(this, status, Toast.LENGTH_LONG);
+            return null;
+        }
+    }
+
+    public class Read extends AsyncTask<ArrayList<String>,Integer , ArrayList<String>>
+    {
+        @Override
+        protected void onPostExecute(ArrayList<String> result) {
+
+            usernameTextView.setText(result.get(0));
+            userIdTextView.setText((result.get(1)));
+            super.onPostExecute(result);
+        }
+
+        @Override
+        protected ArrayList<String> doInBackground(ArrayList<String>... params) {
+            try {
+                json = profileInformation("user");
+                ArrayList<String> results = new ArrayList<String>();
+                results.add(json.getString(params[0].get(0)));
+                results.add(json.getString(params[0].get(1)));
+                return results;
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
     }
 }
