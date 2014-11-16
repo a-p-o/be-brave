d5ace1a108243c00c7821165b01d9f5c0b2b071f
Alex Ordonez <ordonez.alex.pierce@gmail.com>
2014-11-16 18:58:40 -0500

    Fix AlertActivity

--- app/src/main/java/com/ordonezalex/bebrave/AlertActivity.java
+++ app/src/main/java/com/ordonezalex/bebrave/AlertActivity.java
@@ -1,54 +1,39 @@
 package com.ordonezalex.bebrave;
 
 import android.app.Activity;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.Toast;
 
 import com.ordonezalex.bebrave.tasks.GetAlertsTask;
 import com.ordonezalex.bebrave.util.Alert;
-import com.ordonezalex.bebrave.util.School;
-import com.ordonezalex.bebrave.util.Status;
-import com.ordonezalex.bebrave.util.User;
 
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.util.EntityUtils;
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.ExecutionException;
 
-
 public class AlertActivity extends Activity {
+    public final static String TAG = "BeBrave";
 
     private ListView alertListView;
     private Alert[] alerts;
-    private String[] titles;
+    private List<String> titles;
     private final static String URL = "http://caffeinatedcm-001-site3.smarterasp.net/api/v1/alert";
-    private long userSchool = 4;//will be pulled from user
+    private long userSchoolId = 4;//will be pulled from user
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_alerts);
 
-        alertListView = (ListView) findViewById(R.id.alerts_listView);
+        alertListView = (ListView) findViewById(R.id.alerts_list_view);
         populateAlerts();
-
     }
 
     private void populateAlerts() {
-        alerts = new Alert[0];
+
+//        alerts = new Alert[0];
 
         try {
             alerts = new GetAlertsTask().execute().get();
@@ -58,19 +43,19 @@ public class AlertActivity extends Activity {
             e.printStackTrace();
         }
 
-        titles = new String[alerts.length];
+        titles = new ArrayList<String>();
         int count = 0;
 
         //Filter out alerts from other schools
-        for(int i = 0; i < alerts.length; i++){
-            if(alerts[i].getSchool().getId() == userSchool){
-                titles[count] = alerts[i].getTitle();
-                count ++;
+        for (int i = 0; i < alerts.length; i++) {
+            if (alerts[i].getSchool().getId() == userSchoolId) {
+                titles.add(alerts[i].getTitle());
+                count++;
             }
         }
 
         //add alerts to adapter
-        ArrayAdapter adapter = new ArrayAdapter(this, R.layout.listview_alerts, titles);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, titles);
         alertListView.setAdapter(adapter);
     }
 }
