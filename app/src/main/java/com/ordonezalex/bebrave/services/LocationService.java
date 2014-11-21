f5d2b27296cad20a7e645d111f2b937d86f32730
Sebastian Florez <sflorez@stetson.edu>
2014-11-20 21:37:30 -0500

    Add the rest of google location implementation, does not track location cant figure out what is wrong

--- app/src/main/java/com/ordonezalex/bebrave/services/LocationService.java
+++ app/src/main/java/com/ordonezalex/bebrave/services/LocationService.java
@@ -6,6 +6,7 @@ import android.location.Location;
 import android.os.Bundle;
 import android.os.IBinder;
 import android.util.Log;
+import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GooglePlayServicesClient;
@@ -14,14 +15,35 @@ import com.google.android.gms.location.LocationClient;
 import com.google.android.gms.location.LocationListener;
 import com.google.android.gms.location.LocationRequest;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class LocationService extends Service implements GooglePlayServicesClient.ConnectionCallbacks, GooglePlayServicesClient.OnConnectionFailedListener, LocationListener {
 
     private static final String TAG = LocationService.class.getSimpleName();
-    private static final long LOCATION_REQUEST_INTERVAL = 10000;
-    private static final long LOCATION_REQUEST_INTERVAL_FASTEST = 5000;
+    private static final long LOCATION_REQUEST_INTERVAL = 0;
+    private static final long LOCATION_REQUEST_INTERVAL_FASTEST = 0;
 
     LocationClient locationClient;
     LocationRequest locationRequest;
+
+    private Timer timer;
+
+    private TimerTask updateTask = new TimerTask() {
+        @Override
+        public void run() {
+//            Location testLocation = createLocation(100, 100, 50);
+//            locationClient.setMockLocation(testLocation);
+
+            Location newLocation = locationClient.getLastLocation();
+            if( newLocation != null)
+                Log.i(TAG, "position: " + newLocation.getLatitude() + ", " + newLocation.getLongitude());
+            else
+                Log.i(TAG, "null");
+
+            Log.i(TAG, "Timer task doing work");
+        }
+    };
 
     @Override
     public IBinder onBind(Intent intent) {
@@ -31,8 +53,12 @@ public class LocationService extends Service implements GooglePlayServicesClient
 
     @Override
     public void onCreate() {
-
         super.onCreate();
+
+        getLocationUpdates();
+
+        timer = new Timer("LocationServiceTimer");
+        timer.schedule(updateTask, 10L, 60 * 100L);
         Log.i(TAG, "Creating location service");
     }
 
@@ -40,13 +66,13 @@ public class LocationService extends Service implements GooglePlayServicesClient
     public void onDestroy() {
 
         super.onDestroy();
+        timer.cancel();
+        timer = null;
         Log.i(TAG, "Destroying location service");
     }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-
-        this.getLocationUpdates();
 
         return super.onStartCommand(intent, flags, startId);
     }
@@ -62,34 +88,78 @@ public class LocationService extends Service implements GooglePlayServicesClient
         } else {
             Log.wtf(TAG, "Cannot connect to Google Play Services. Check your network connection.");
         }
+
+    }
+
+    private void stopLocationUpdates()
+    {
+        if(locationClient != null && locationClient.isConnected())
+        {
+            locationClient.removeLocationUpdates(this);
+            locationClient.disconnect();
+        }
     }
 
     @Override
     public void onConnected(Bundle bundle) {
 
         Log.i(TAG, "Connected to service.");
+        Toast.makeText( this,"Connected to service" , Toast.LENGTH_SHORT).show();
 
         locationRequest = LocationRequest.create();
         locationRequest.setInterval(LOCATION_REQUEST_INTERVAL);
-        locationRequest.setInterval(LOCATION_REQUEST_INTERVAL_FASTEST);
-        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        locationRequest.setFastestInterval(LOCATION_REQUEST_INTERVAL_FASTEST);
+        locationRequest.setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY);
+        locationRequest.setSmallestDisplacement(0);
 
-        locationClient.requestLocationUpdates(locationRequest, this);
+       // locationClient.setMockMode(true);
+        locationClient.requestLocationUpdates(locationRequest, this , getMainLooper());
+
+
+
     }
 
     @Override
     public void onDisconnected() {
 
+        Log.e(TAG, "Disconnected");
+        stopSelf();
     }
 
     @Override
     public void onLocationChanged(Location location) {
+        if(location != null)
+        {
+            Log.i(TAG,"position: " + location.getLatitude() + ", " + location.getLongitude());
+            Toast.makeText(this, "position: " + location.getLatitude() + ", " + location.getLongitude(), Toast.LENGTH_SHORT).show();
+        }
+        else
+        {
+            Log.i(TAG, "Location is null");
+
+            Toast.makeText( this,"Location is null" , Toast.LENGTH_SHORT).show();
+        }
+
+        Log.i(TAG, "Location should be tracked gg ");
 
     }
 
     @Override
     public void onConnectionFailed(ConnectionResult connectionResult) {
 
+        Log.e(TAG, "ConnectionFailed");
+        stopLocationUpdates();
+        stopSelf();
+
+    }
+
+    public Location createLocation( double lat , double lng, float accuracy)
+    {
+        Location newLocation = new Location("be-brave");
+        newLocation.setLatitude(lat);
+        newLocation.setLongitude(lng);
+        newLocation.setAccuracy(accuracy);
+        return newLocation;
     }
 }
 
--- app/src/main/java/com/ordonezalex/bebrave/tasks/CreateUserTask.java
+++ app/src/main/java/com/ordonezalex/bebrave/tasks/CreateUserTask.java
@@ -1,56 +1,56 @@
-package com.ordonezalex.bebrave.tasks;
-
-import android.os.AsyncTask;
-import android.os.Message;
-import android.util.Log;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.ordonezalex.bebrave.util.User;
-
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.http.converter.StringHttpMessageConverter;
-import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
-import org.springframework.web.client.HttpClientErrorException;
-import org.springframework.web.client.HttpServerErrorException;
-import org.springframework.web.client.RestTemplate;
-
-public class CreateUserTask extends AsyncTask<User, Void, String> {
-    public final static String TAG = "BeBrave";
-    private final static String URL = "http://caffeinatedcm-001-site3.smarterasp.net/api/v1/User";
-
-    @Override
-    protected String doInBackground(User... users) {
-
-        User user;
-        uesr = users[0];
-        Message message = new Message();
-
-        HttpHeaders requestHeaders = new HttpHeaders();
-        requestHeaders.setContentType(new MediaType("application", "json"));
-        HttpEntity<User> requestEntity = new HttpEntity<User>(users, requestHeaders);
-
-        ObjectMapper mapper = new ObjectMapper();
-
-        RestTemplate restTemplate = new RestTemplate();
-
-        restTemplate.getMessageConverters().add(new MappingJackson2HttpMessageConverter());
-        restTemplate.getMessageConverters().add(new StringHttpMessageConverter());
-
-        String result = null;
-
-        try {
-            ResponseEntity<String> response = restTemplate.exchange(URL, HttpMethod.POST, requestEntity, String.class);
-            result = response.getBody();
-        } catch (HttpServerErr orException e) {
-            Log.wtf(TAG, e.getResponseBodyAsString());
-        } catch (HttpClientErrorException e) {
-            Log.wtf(TAG, e);
-        }
-
-        return result;
-    }
-}
+//package com.ordonezalex.bebrave.tasks;
+//
+//import android.os.AsyncTask;
+//import android.os.Message;
+//import android.util.Log;
+//
+//import com.fasterxml.jackson.databind.ObjectMapper;
+//import com.ordonezalex.bebrave.util.User;
+//
+//import org.springframework.http.HttpEntity;
+//import org.springframework.http.HttpHeaders;
+//import org.springframework.http.HttpMethod;
+//import org.springframework.http.MediaType;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.http.converter.StringHttpMessageConverter;
+//import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+//import org.springframework.web.client.HttpClientErrorException;
+//import org.springframework.web.client.HttpServerErrorException;
+//import org.springframework.web.client.RestTemplate;
+//
+//public class CreateUserTask extends AsyncTask<User, Void, String> {
+//    public final static String TAG = "BeBrave";
+//    private final static String URL = "http://caffeinatedcm-001-site3.smarterasp.net/api/v1/User";
+//
+//    @Override
+//    protected String doInBackground(User... users) {
+//
+//        User user;
+//        uesr = users[0];
+//        Message message = new Message();
+//
+//        HttpHeaders requestHeaders = new HttpHeaders();
+//        requestHeaders.setContentType(new MediaType("application", "json"));
+//        HttpEntity<User> requestEntity = new HttpEntity<User>(users, requestHeaders);
+//
+//        ObjectMapper mapper = new ObjectMapper();
+//
+//        RestTemplate restTemplate = new RestTemplate();
+//
+//        restTemplate.getMessageConverters().add(new MappingJackson2HttpMessageConverter());
+//        restTemplate.getMessageConverters().add(new StringHttpMessageConverter());
+//
+//        String result = null;
+//
+//        try {
+//            ResponseEntity<String> response = restTemplate.exchange(URL, HttpMethod.POST, requestEntity, String.class);
+//            result = response.getBody();
+//        } catch (HttpServerErr orException e) {
+//            Log.wtf(TAG, e.getResponseBodyAsString());
+//        } catch (HttpClientErrorException e) {
+//            Log.wtf(TAG, e);
+//        }
+//
+//        return result;
+//    }
+//}
