b1a84c64da57e1a27e4853b8290cba14f94ef9c9
Sebastian Florez <sflorez@stetson.edu>
2014-11-24 22:39:47 -0500

    Fix alerts were not populating due to changes of ids and the new name property of the school enitity. Got rid of the latancy when loading up the alerts activity due to the .get at the end of the execute for the task. Fixed logic due to those changes.

--- app/src/main/java/com/ordonezalex/bebrave/AlertActivity.java
+++ app/src/main/java/com/ordonezalex/bebrave/AlertActivity.java
@@ -9,6 +9,8 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.ordonezalex.bebrave.tasks.CreateReportsTask;
 import com.ordonezalex.bebrave.tasks.GetAlertsTask;
 import com.ordonezalex.bebrave.util.Alert;
@@ -24,10 +26,10 @@ import java.util.concurrent.ExecutionException;
 public class AlertActivity extends Activity {
 
     public final static String TAG = "BeBrave";
-    private  List<String> titles;
-    private ListView alertListView;
+    public  List<String> titles;
+    public ListView alertListView;
     private Alert[] alerts;
-    private static long userSchoolId = 4; // Will be pulled from user
+    public static long userSchoolId = 3; // Will be pulled from user
     private final static String URL = "http://caffeinatedcm-001-site3.smarterasp.net/api/v1/alert?School=" + userSchoolId;
 
     @Override
@@ -42,27 +44,9 @@ public class AlertActivity extends Activity {
     }
 
     private void populateAlerts() {
-
         titles = new ArrayList<String>();
+        new GetAlertsTask(this).execute();
 
-        try {
-            alerts = new GetAlertsTask().execute().get();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        } catch (ExecutionException e) {
-            e.printStackTrace();
-        }
-
-        // Filter out Alerts from other schools
-        for (Alert alert : alerts) {
-            if (alert.getSchool().getId() == userSchoolId) {
-                titles.add(alert.getTitle());
-            }
-        }
-
-        // Add Alerts to adapter
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, titles);
-        alertListView.setAdapter(adapter);
     }
 
     private void alertClicked() {
@@ -86,22 +70,25 @@ public class AlertActivity extends Activity {
 
         // Get Android school
         School school = new School();
-        school.setId(4);
+        school.setId(3);
 
         // Get Help me status
         Status status = new Status();
-        status.setId(1);
+        status.setId(2);
 
         // Get textUser User
         User user = new User();
-        user.setId(1);
+        user.setId(2);
 
         Report report = new Report();
         report.setUser(user);
         report.setAlert(alert);
         report.setStatus(status);
         report.setSchool(school);
+
+        ObjectMapper mapper = new ObjectMapper();
         try{
+            Log.i(TAG, mapper.writerWithDefaultPrettyPrinter().writeValueAsString(report) );
             String response = new CreateReportsTask(this).execute(report).get();
             Log.wtf(TAG, response);
         } catch (InterruptedException e) {
@@ -110,7 +97,13 @@ public class AlertActivity extends Activity {
         } catch (ExecutionException e) {
             e.printStackTrace();
             Log.e(TAG, e.toString());
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
         }
+    }
+
+    public void setAlerts(Alert[] alerts) {
+        this.alerts = alerts;
     }
 }
 
--- app/src/main/java/com/ordonezalex/bebrave/tasks/GetAlertsTask.java
+++ app/src/main/java/com/ordonezalex/bebrave/tasks/GetAlertsTask.java
@@ -1,7 +1,14 @@
 package com.ordonezalex.bebrave.tasks;
 
+import android.annotation.TargetApi;
+import android.app.Activity;
+import android.app.Notification;
 import android.os.AsyncTask;
+import android.os.Build;
+import android.widget.ArrayAdapter;
 
+import com.ordonezalex.bebrave.AlertActivity;
+import com.ordonezalex.bebrave.R;
 import com.ordonezalex.bebrave.util.Alert;
 
 import org.springframework.http.HttpEntity;
@@ -17,6 +24,22 @@ import java.util.Collections;
 public class GetAlertsTask extends AsyncTask<Void, Void, Alert[]> {
     public final static String TAG = "BeBrave";
     private final static String URL = "http://caffeinatedcm-001-site3.smarterasp.net/api/v1/Alert";
+    private AlertActivity activity;
+    public final static int NOTIFICATION_REPORT_SENDING_ID = 2;
+    private Notification.Builder builder;
+
+    @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
+    public GetAlertsTask(AlertActivity activity) {
+
+        super();
+        this.setActivity(activity);
+        this.builder = new Notification.Builder(activity)
+                .setSmallIcon(R.drawable.ic_launcher)
+                .setContentTitle(activity.getResources().getString(R.string.notification_share_walk_title))
+                .setContentText(activity.getResources().getString(R.string.notification_share_walk_text))
+                .setOngoing(true)
+                .setPriority(NOTIFICATION_REPORT_SENDING_ID);
+    }
 
     @Override
     protected Alert[] doInBackground(Void... voids) {
@@ -36,5 +59,24 @@ public class GetAlertsTask extends AsyncTask<Void, Void, Alert[]> {
         Alert[] alert = responseEntity.getBody();
 
         return alert;
+    }
+
+    @Override
+    protected void onPostExecute(Alert[] alerts) {
+
+        for (Alert alert : alerts) {
+            if (alert.getSchool().getId() == this.activity.userSchoolId) {
+                this.activity.titles.add(alert.getTitle());
+            }
+        }
+
+        // Add Alerts to adapter
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this.activity, android.R.layout.simple_list_item_1, this.activity.titles);
+        this.activity.alertListView.setAdapter(adapter);
+        super.onPostExecute(alerts);
+    }
+
+    public void setActivity(AlertActivity activity) {
+        this.activity = activity;
     }
 }
